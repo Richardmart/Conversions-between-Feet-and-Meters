8378ae32bcb45cc7cfc85c3e5120127d6704dcde
Richard Martinez <richardmart09@hotmail.com>
2023-08-27 21:46:16 -0400

    First commit

---
+++ src/ConvertFeetAndMeters.java
@@ -0,0 +1,12 @@
+/*
+ * Richard Martinez
+ * Date 8/27/2023
+ * Chapter 6 Assignment 6.9 Conversions between Feet and Meters
+ * Program converts feet and meters into the opposite and displays results in a table
+ * using the two methods footToMeter and meterToFoot
+ */
+public class ConvertFeetAndMeters {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    } 
+}
