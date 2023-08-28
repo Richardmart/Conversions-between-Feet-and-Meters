3a51af5b0b08752cdd5f5ffdd8477d77e482e566
Richard Martinez <richardmart09@hotmail.com>
2023-08-27 22:18:50 -0400

    Fourth Commit

--- src/ConvertFeetAndMeters.java
+++ src/ConvertFeetAndMeters.java
@@ -6,13 +6,18 @@
  * using the two methods footToMeter and meterToFoot
  */
 public class ConvertFeetAndMeters {
-    public static void main(String[] args) throws Exception {
-        double testNumber = 1.0;
+    public static void main(String[] args) {
+        double testNumber = 10.0;
         double convertedNumber = footToMeter(testNumber);
-        System.out.print(convertedNumber);
+        // System.out.format("%.2f", convertedNumber);
+        System.out.printf("%.3f\n", convertedNumber);
+        double newConvertedNumber = meterToFoot(testNumber);
+        // System.out.format("%.2f", newConvertedNumber);
+        System.out.printf("%.3f\n", newConvertedNumber);
+
     } 
 
-    public static double footToMeter(double foot){
+    public static double footToMeter(double foot) {
         // Create the constant for feet in meter
         final double FEET_IN_METER = 0.305;
         // Convert feet to meters
@@ -21,9 +26,13 @@ public class ConvertFeetAndMeters {
         return conversion;
     }
 
-    public static double meterToFoot(double meter){
-        meter += 5;
-        return meter;
+    public static double meterToFoot(double meter) {
+        // Create the constant for meter in feet
+        final double METER_IN_FEET = 3.279;
+        // Convert meters to feet
+        double conversion = meter * METER_IN_FEET;
+        // Return conversion
+        return conversion;
     }
 
 
